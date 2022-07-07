130647d935210a686a78d8ecfa8a9b66c03305e4
emachizy <emachi2011@gmail.com>
2022-07-07 13:50:33 -0700

    More commit for functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *mul - returns the multiplied parameters
+  *@a: int type number
+  @b: int type number
+  *Return: always sucess 0
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
