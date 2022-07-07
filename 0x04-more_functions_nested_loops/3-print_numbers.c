4e2030904f81032a1b94696aaaafb149a3f4f61d
emachizy <emachi2011@gmail.com>
2022-07-07 13:57:21 -0700

    prints numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *main - prints numbers from 0 - 9
+  *
+  *Return: always sucess 0
+  */
+
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
