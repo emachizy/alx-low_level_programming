04eaf566f42273d3c2749717377b794c9fb9ac3a
emachizy <emachi2011@gmail.com>
2022-07-06 17:36:41 -0700

    main files and header files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,29 @@
-#include "main.h"
+include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
-int main(void)
-{
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
-}
+0 int main(void)
+ 11 {
+ 12         int n;
+ 13 
+ 14         srand(time(0));
+ 15         n = rand() - RAND_MAX / 2;
+ 16 
+ 17         if (n > 0)
+ 18         {
+ 19                 printf("%i is positive\n", n);
+ 20         }
+ 21         else if (n == 0)
+ 22         {
+ 23                 printf("%i is zero\n", n);
+ 24         }
+ 25         else if (n < 0)
+ 26         {
+ 27                 printf("%i is negative\n", n);
+ 28         }
+ 29 
+ 30         return (0);
+ 31 }
