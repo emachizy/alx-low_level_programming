cb12e90f5543a2ad7a60399c4bbf2ebf78df94d5
emachizy <emachi2011@gmail.com>
2022-07-07 06:12:39 -0700

    comment out error

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  *main - cause an infinite loop
+  *Return: sucess always (0)
+  */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = o;
+
+	/**
+	  *while (i < 10)
+	  *{
+	  *	putchar(i);
+	  *}
+	  */
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
