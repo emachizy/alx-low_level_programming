49b9537331d3c925c63ff27307836dab90f45c69
emachizy <emachi2011@gmail.com>
2022-07-14 17:32:38 -0700

    Reversing the content of an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * _strcmp - compares 2 strings
- * @s1: 1st string
- * @s2: 2nd string
- * Return: 0 if matching, and ns1 - ns2 if not matching
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i;
-
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-	}
-
-	return (0);
-#include "main.h"
-
-/**
  * reverse_array - reverse a int array
  * @a: pointer
  * @n: array length
@@ -37,4 +16,4 @@ void reverse_array(int *a, int n)
 		a[i] = a[n - i - 1] - a[i];
 		a[n - i - 1] = a[n - i - 1] - a[i];
 	}
-}}
+}
