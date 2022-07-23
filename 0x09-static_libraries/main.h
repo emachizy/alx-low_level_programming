0c02a2b737eae546caaf10b494d071d945adab7a
emachizy <emachi2011@gmail.com>
2022-07-22 18:53:09 -0700

    static library

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
+#define MAIN_H
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
