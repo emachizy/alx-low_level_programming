ff1e91e247d5a75358ff2e1d05670811c38c42d4
emachizy <emachi2011@gmail.com>
2022-08-01 15:46:19 -0700

    function-like macro that computes sum

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef _FUNCTION_LIKE_MACRO
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
 
-#define _FUNCTION_LIKE_MACRO
-
-#define ABS(x) (x)
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
+
